166178c4f272283238660787e6201984280a46cf
danielguerra69 <daniel.guerra69@gmail.com>
2016-02-08 00:10:09 +0100

    now really without the kali-root dir

--- build-kali.sh
+++ build-kali.sh
@@ -3,5 +3,6 @@
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=refs/heads/kali/master" > kali-debootstrap &&\
 debootstrap kali-current ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
-tar cfvz /mnt/kali-root.tar.gz kali-root/*  &&\
+cd kali-root &&\
+tar cfvz /mnt/kali-root.tar.gz *  &&\
 echo "Build OK" || echo "Build failed!"
