129c8062c7ac085f4d868926e7ce77f31ff21ae2
danielguerra69 <daniel.guerra69@gmail.com>
2016-02-09 21:43:23 +0100

    add kali gpg key

--- build-kali.sh
+++ build-kali.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Fetch the latest Kali debootstrap script from git
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6 &&\
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=refs/heads/kali/master" > kali-debootstrap &&\
 debootstrap kali-current ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
 cd kali-root &&\
